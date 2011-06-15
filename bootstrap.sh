7ce0716c716f77e839df379ab90aa64875dc38af
Frederic Jean <fred@fredjean.net>
2011-06-15 16:37:51 -0600

    Removing the --remote option now that we have our own name.

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,10 +15,10 @@ fi
 which gem | grep -q rvm
 if [ $? -eq 0 ]; then
   gem uninstall snuggie -aIx              >> ~/.snuggie/bootstrap.log 2>&1
-  gem install   snuggie --no-rdoc --no-ri --remote --source http://gems.snugghome.com >> ~/.snuggie/bootstrap.log 2>&1
+  gem install   snuggie --no-rdoc --no-ri --source http://gems.snugghome.com >> ~/.snuggie/bootstrap.log 2>&1
 else
   sudo gem uninstall snuggie -aIx              >> ~/.snuggie/bootstrap.log 2>&1
-  sudo gem install   snuggie --no-rdoc --no-ri --remote --source http://gems.snugghome.com >> ~/.snuggie/bootstrap.log 2>&1
+  sudo gem install   snuggie --no-rdoc --no-ri --source http://gems.snugghome.com >> ~/.snuggie/bootstrap.log 2>&1
   sudo gem update                                 >> ~/.snuggie/bootstrap.log 2>&1
 fi
 
